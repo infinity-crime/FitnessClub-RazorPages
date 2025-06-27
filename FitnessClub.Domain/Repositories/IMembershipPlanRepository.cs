56242ce2e45b61fbcae263c86f36ef1ded6ecf6b
Kirill Zhestkov <infinity.crime@outlook.com>
2025-06-27 16:01:17 +0300

    Added repositories for each DB table.

---
+++ FitnessClub.Domain/Repositories/IMembershipPlanRepository.cs
@@ -0,0 +1,20 @@
+﻿using FitnessClub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Domain.Repositories
+{
+    public interface IMembershipPlanRepository
+    {
+        Task<MembershipPlan?> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<MembershipPlan>?> GetAllAsync();
+
+        Task AddAsync(MembershipPlan plan);
+
+        Task Update(MembershipPlan plan);
+    }
+}
---
+++ FitnessClub.Domain/Repositories/ISubscriptionRepository.cs
@@ -0,0 +1,20 @@
+﻿using FitnessClub.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Domain.Repositories
+{
+    public interface ISubscriptionRepository
+    {
+        Task<Subscription?> GetByIdAsync(Guid id);
+
+        Task<IEnumerable<Subscription>?> GetByUserIdAsync(Guid userId);
+
+        Task Add(Subscription subscription);
+
+        Task Update(Subscription subscription);
+    }
+}
