955a57c33969ede5acd8362572dcf609d7666832
Kirill Zhestkov <infinity.crime@outlook.com>
2025-06-27 14:58:20 +0300

    The basic preparation of domain layer entities is complete.

---
+++ FitnessClub.Domain/Entities/MembershipPlan.cs
@@ -0,0 +1,47 @@
+﻿using FitnessClub.Domain.ValueObjects;
+using FitnessClub.Domain.Exceptions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Domain.Entities
+{
+    public class MembershipPlan
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = string.Empty;
+        public string Description { get; private set; } = string.Empty;
+        public Money Price { get; private set; }
+        public int DurationInMonths { get; private set; }
+
+        private MembershipPlan(Guid id, string name, string description, Money price, int durationInMonths)
+        {
+            if (id == Guid.Empty)
+                throw new DomainException("The plan ID must be non-zero!");
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(description))
+                throw new DomainException("Plan name/description is required!");
+
+            if(durationInMonths <= 0)
+                throw new DomainException("Duration must be greater than zero!");
+
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            DurationInMonths = durationInMonths;
+        }
+
+        public static MembershipPlan Create(string name, string description, decimal amountPrice, 
+            string currencyPrice, int durationInMonths)
+        {
+            var id = Guid.NewGuid();
+            var price = Money.Create(amountPrice, currencyPrice);
+
+            return new MembershipPlan(id, name, description, price, durationInMonths);
+        }
+    }
+}
---
+++ FitnessClub.Domain/Entities/Subscription.cs
@@ -0,0 +1,59 @@
+﻿using FitnessClub.Domain.Exceptions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Domain.Entities
+{
+    public class Subscription
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid MembershipPlanId { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public SubscriptionStatus Status { get; private set; }
+        public DateTime? LastModifiedDate { get; private set; }
+
+        private Subscription(Guid id, Guid userId, Guid membershipPlanId, DateTime startDate, 
+            DateTime endDate, SubscriptionStatus status)
+        {
+            if ((id == Guid.Empty) || (userId == Guid.Empty) || (membershipPlanId == Guid.Empty))
+                throw new DomainException("Subscription id/userId/planId is required!");
+
+            if (startDate >= endDate)
+                throw new DomainException("The subscription start date cannot be greater than or equal to the subscription end date!");
+
+            Id = id;
+            UserId = userId;
+            MembershipPlanId = membershipPlanId;
+            StartDate = startDate;
+            EndDate = endDate;
+            Status = status;
+            LastModifiedDate = DateTime.UtcNow;
+        }
+
+        public static Subscription Create(User user, MembershipPlan plan)
+        {
+            if ((user is null) || (plan is null))
+                throw new DomainException("User/Membership_plan is required!");
+
+            var id = Guid.NewGuid();
+            var startDate = DateTime.Now;
+            var endDate = startDate.AddMonths(plan.DurationInMonths);
+
+            return new Subscription(id, user.Id, plan.Id, startDate, endDate, SubscriptionStatus.Active);
+        }
+
+        public enum SubscriptionStatus
+        {
+            Active, // активна 
+            Frozen, // заморожена
+            Expired, // закончилась
+            Canceled // отменена
+        }
+    }
+}
